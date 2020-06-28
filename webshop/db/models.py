f44634326eb3b455c395040e72848ca9b950cd2e
Melton <melton97zd@gmail.com>
2020-06-28 23:10:15 +0300

    5

--- webshop/db/models.py
+++ webshop/db/models.py
@@ -1,6 +1,33 @@
-from mongoengine import *
+import mongoengine as me
+import datetime
 
-connect('bot_db')
+me.connect('bot_db')
 
-class Products(Document):
-    pass
+
+class Category(me.Document):
+    title = me.StringField(min_length=1, max_length=512)
+    description = me.StringField(min_length=2, max_length=4096)
+    subcategories = me.ListField(me.ReferenceField('self'))
+    parent = me.ReferenceField('self', default=None)
+
+
+
+class Products(me.Document):
+    title = me.StringField(min_length=1, max_length=512)
+    description = me.StringField(min_length=2 ,max_length=4096)
+    created = me.DateField(default=datetime.datetime.now())
+    price = me.DecimalField(required=True)
+    in_stock = me.BooleanField(default=True)
+    discount = me.IntField(min_value=0, max_value=100)
+    image = me.FileField(required=True)
+    category = me.ReferenceField(Category)
+
+
+class Text(me.Document):
+
+    TITLES = {
+        'greetings': 'Текст приветствия',
+        'cart': 'Текст корзины'
+    }
+    title = me.StringField(min_length=1, max_length=256, choices=TITLES.values(), unique=True)
+    body = me.StringField(min_length=1, max_length=4096)
